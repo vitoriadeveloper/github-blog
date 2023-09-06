2ac7a8a4a575dedfef2f58a7b4d20bf7b2adfe6f
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-09-05 21:26:22 -0300

    condition changed

--- src/pages/About/components/Content/index.tsx
+++ src/pages/About/components/Content/index.tsx
@@ -4,49 +4,36 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import { useContextSelector } from "use-context-selector";
-import { PostContext } from "../../../../contexts/PostContext";
-import { Spinner } from "../../../../components/Spinner";
 
 interface PostContentProps {
     content: string;
 }
 
 export function PostContent({ content }: PostContentProps) {
-    const isLoading = useContextSelector(PostContext, (context) => {
-        return context.isLoading;
-    });
     return (
         <div>
-            {!isLoading && (
-                <ReactMarkdown
-                    components={{
-                        code({ node, inline, className, children, ...props }) {
-                            const match = /language-(\w+)/.exec(
-                                className || "",
-                            );
-                            return !inline && match ? (
-                                <SyntaxHighlighter
-                                    children={String(children).replace(
-                                        /\n$/,
-                                        "",
-                                    )}
-                                    style={dracula as any}
-                                    language={match[1]}
-                                    PreTag="div"
-                                    {...props}
-                                />
-                            ) : (
-                                <code className={className} {...props}>
-                                    {children}
-                                </code>
-                            );
-                        },
-                    }}
-                >
-                    {content}
-                </ReactMarkdown>
-            )}
+            <ReactMarkdown
+                components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || "");
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                children={String(children).replace(/\n$/, "")}
+                                style={dracula as any}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        );
+                    },
+                }}
+            >
+                {content}
+            </ReactMarkdown>
         </div>
     );
 }
